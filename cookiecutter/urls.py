2f7c5912c4413057ae794dbf20a523f8b8c3fd70
Godfrey-Ndungu <godmak2022@gmail.com>
2023-03-30 00:11:09 +0300

    Add djangorestframework-simplejwt==5.2.2

--- cookiecutter/urls.py
+++ cookiecutter/urls.py
@@ -2,10 +2,18 @@ from django.contrib import admin
 from django.urls import path, re_path,include
 from django.conf import settings
 from django.views.static import serve
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+from rest_framework_simplejwt.views import TokenVerifyView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('api-auth/', include('rest_framework.urls'))
+    path('api-auth/', include('rest_framework.urls')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
 
 if settings.DEBUG:
