18b995003f7b64878044070886879892e6b18b3a
Godfrey-Ndungu <godmak2022@gmail.com>
2023-03-29 23:44:09 +0300

    Configure static in development settings and add boto and django storages to handle S3 buckets in settings file
     This commit updates the development settings file to properly configure the static files. Additionally, it adds Boto and Django Storages to the project's dependencies, which will allow the application to handle S3 buckets for storing static and media files.

--- cookiecutter/urls.py
+++ cookiecutter/urls.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
+from django.conf import settings
+from django.views.static import serve
 
 urlpatterns = [
     path("admin/", admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += [
+        re_path("media/(?P<path>.*)", serve,
+                {"document_root": settings.MEDIA_ROOT})
+    ]
