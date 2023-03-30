e45a055fc72753df8219c604cdb70db0c39129af
Godfrey-Ndungu <godmak2022@gmail.com>
2023-03-30 11:50:03 +0300

    feat: Add drf-spectacular for API documentation==0.26.1

--- cookiecutter/urls.py
+++ cookiecutter/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path, re_path,include
+from django.urls import path, re_path, include
 from django.conf import settings
 from django.views.static import serve
 from rest_framework_simplejwt.views import (
@@ -7,17 +7,25 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 from rest_framework_simplejwt.views import TokenVerifyView
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('api-auth/', include('rest_framework.urls')),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path("api-auth/", include("rest_framework.urls")),
+    path("api/token/",
+         TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/",
+         TokenRefreshView.as_view(), name="token_refresh"),
+    path("api/token/verify/",
+         TokenVerifyView.as_view(), name="token_verify"),
+    path('api/schema/',
+         SpectacularAPIView.as_view(), name='schema'),
+    path('api/schema/redoc/',
+         SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
 
 if settings.DEBUG:
     urlpatterns += [
-        re_path("media/(?P<path>.*)", serve,
-                {"document_root": settings.MEDIA_ROOT})
+        re_path("media/(?P<path>.*)",
+                serve, {"document_root": settings.MEDIA_ROOT})
     ]
