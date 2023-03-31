bd281daf4bb3aba1b03af20b8715412b50cbcbb7
Godfrey-Ndungu <godmak2022@gmail.com>
2023-03-31 14:07:13 +0300

    feat(docs): Add and configure Sphinx documentation
    This commit adds Sphinx documentation to the Django project to provide better documentation for users and developers. The following changes were made:
    
    - Added  and  as dependencies in
    - Created a new  directory to contain the Sphinx configuration and documentation files
    - Added  files to the  directory to configure Sphinx

---
+++ docs/source/conf.py
@@ -0,0 +1,21 @@
+import os
+import sys
+import django
+
+sys.path.insert(0, os.path.abspath("../../"))
+os.environ["DJANGO_SETTINGS_MODULE"] = "cookiecutter.development"
+django.setup()
+
+project = "django-minimal-cookiecutter"
+copyright = "2023, godfrey-ndungu"
+author = "godfrey-ndungu"
+release = "1.0.0"
+
+extensions = ["sphinx.ext.autodoc",
+              "sphinx.ext.viewcode", "sphinx.ext.napoleon"]
+
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
