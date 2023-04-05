734ab3fc6336b0e05ab07bf73e6809990ace1cbd
Godfrey-Ndungu <godmak2022@gmail.com>
2023-04-05 23:59:04 +0300

    Add seperate requirements for sphinx and readthedocs config yaml

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,6 +2,17 @@ import os
 import sys
 import django
 
+
+requirements_path = os.path.join(os.path.dirname(__file__), 'requirements.txt')
+
+# Install the requirements
+try:
+    with open(requirements_path) as f:
+        install_reqs = f.read()
+    os.system(f"pip install {install_reqs}")
+except FileNotFoundError:
+    print("Requirements file not found. Skipping install.")
+
 sys.path.insert(0, os.path.abspath("../../"))
 os.environ["DJANGO_SETTINGS_MODULE"] = "cookiecutter.development"
 django.setup()
