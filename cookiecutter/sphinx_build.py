dabfb242df0721b9af8901b32dc7c5118a9b1f89
Godfrey-Ndungu <godmak2022@gmail.com>
2023-04-06 12:37:00 +0300

    Add settings for sphinx docs

---
+++ cookiecutter/sphinx_build.py
@@ -0,0 +1,20 @@
+from split_settings.tools import include
+from pathlib import Path
+from decouple import config
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+include(
+    "components/base.py",
+)
+
+ALLOWED_HOSTS = ["*"]
+
+DEBUG = True
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = "media/"
+
+CELERY_BROKER_URL = "redis://localhost:6379"
+CELERY_RESULT_BACKEND = "redis://localhost:6379"
+
+
