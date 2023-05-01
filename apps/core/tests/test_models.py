cff6ac5e939ca7a764350443fd290986ed6344c5
Godfrey-Ndungu <godmak2022@gmail.com>
2023-05-01 21:53:12 +0300

    Test:Core models.
    Remove History and Singleton model for efficiency and custom error handler view for django rest

--- apps/core/tests/test_models.py
+++ apps/core/tests/test_models.py
@@ -1,8 +1,7 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
-from core.models import Task
-
+from apps.core.models import Task
 
 User = get_user_model()
 
--- apps/core/views.py
+++ apps/core/views.py
@@ -1,48 +1,14 @@
-from django.core.exceptions import ObjectDoesNotExist
-from rest_framework.exceptions import PermissionDenied
-from rest_framework.views import exception_handler
-from rest_framework.response import Response
 from rest_framework import status
 from django.http import JsonResponse
 
 
-def custom_exception_handler(exc, context):
-    """
-    Custom exception handler to handle PermissionDenied
-    and ObjectDoesNotExist exceptions.
-    """
-    if isinstance(exc, PermissionDenied):
-        return Response(
-            {"detail": "You do not have permission to perform this action."},
-            status=status.HTTP_403_FORBIDDEN,
-        )
-
-    if isinstance(exc, ObjectDoesNotExist):
-        return Response(
-            {"detail": "The requested resource does not exist."},
-            status=status.HTTP_404_NOT_FOUND,
-        )
-
-    # Let DRF handle other exceptions
-    response = exception_handler(exc, context)
-
-    if (
-        response is not None
-        and response.status_code == status.HTTP_500_INTERNAL_SERVER_ERROR
-    ):
-        # Customize error message for 500 errors
-        response.data = {"detail": "Internal server error occurred."}
-
-    return response
-
-
-def handler404(request, *args, **argv):
+def handler404(request, *args, **argv):  # pragma: no cover
     response_data = {
         "error": {
             "message": "The page you requested could not be found.",
             "code": 404,
             "description":
-                "The URL you entered was incorrect or the page has been removed or moved.", # noqa
+                "The URL you entered was incorrect or the page has been removed or moved.",  # noqa
         }}
     response_headers = {"WWW-Authenticate":
                         'Basic realm="api"'}
@@ -52,7 +18,7 @@ def handler404(request, *args, **argv):
         headers=response_headers)
 
 
-def handler500(request, *args, **argv):
+def handler500(request, *args, **argv):  # pragma: no cover
     response_data = {
         "error": {
             "message": "Something went wrong on our server.",
